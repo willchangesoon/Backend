06c004bbdf822a87675bb07677f7c9394d615bf0
Eunseo-415 <eunseo@lawalliance.co.kr>
2024-12-12 17:21:18 +0900

    init

---
+++ es3-backend/src/main/java/com/es3/es3backend/Es3BackendApplication.java
@@ -0,0 +1,13 @@
+package com.es3.es3backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Es3BackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Es3BackendApplication.class, args);
+    }
+
+}
---
+++ es3-backend/src/test/java/com/es3/es3backend/Es3BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.es3.es3backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Es3BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
